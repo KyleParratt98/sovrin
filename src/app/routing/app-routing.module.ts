7c0fa70662be5f2506b1c3163d34d7d0cd12e25e
Tim Louw <timlouw.8@gmail.com>
2018-05-10 21:23:00 +0200

    Added routes to pages

--- src/app/routing/app-routing.module.ts
+++ src/app/routing/app-routing.module.ts
@@ -7,6 +7,10 @@ import { MainViewComponent } from '../views/main-view/main-view.component';
 import { AdminViewComponent } from '../views/admin-view/admin-view.component';
 import { HomeComponent } from '../views/main-view/home/home.component';
 import { ContactUsComponent } from '../views/main-view/contact-us/contact-us.component';
+import { AboutUsComponent } from '../views/main-view/about-us/about-us.component';
+import { BookOnlineComponent } from '../views/main-view/book-online/book-online.component';
+import { FaqComponent } from '../views/main-view/faq/faq.component';
+import { MyAccountComponent } from '../views/main-view/my-account/my-account.component';
 
 
 const appRoutes: Routes = [
@@ -17,6 +21,10 @@ const appRoutes: Routes = [
             {path: 'home', component: HomeComponent, outlet: 'mainViews'},
             {path: '', redirectTo: '/mv/(mainViews:home)', pathMatch: 'full'},
             {path: 'contact-us', component: ContactUsComponent, outlet: 'mainViews'},
+            {path: 'about-us', component: AboutUsComponent, outlet: 'mainViews'},
+            {path: 'my-account', component: MyAccountComponent, outlet: 'mainViews'},
+            {path: 'faq', component: FaqComponent, outlet: 'mainViews'},
+            {path: 'book-online', component: BookOnlineComponent, outlet: 'mainViews'},
           ]},
           
         { path: 'admin-view', component: AdminViewComponent,
