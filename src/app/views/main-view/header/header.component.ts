960be3c52398539e6936757f7694ee37dff23ae9
Tim Louw <timlouw.8@gmail.com>
2018-06-09 16:30:08 +0200

    Updated the colours and icons in the navbar

--- src/app/views/main-view/header/header.component.ts
+++ src/app/views/main-view/header/header.component.ts
@@ -7,32 +7,10 @@ import { Router } from '@angular/router';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  burgerIcon = 'menu';
-  burgerDisabled = false;
 
   constructor(private router: Router,) { }
 
   ngOnInit() {
-  }
-
-  async burgerToggle() {
-    await this.delay(1);
-    this.burgerDisabled = true;
-    await this.delay(180);
-    console.log('burger toggle');
-    if (this.burgerIcon == 'menu')
-      this.burgerIcon = 'close';
-    else 
-      this.burgerIcon = 'menu';
-    this.burgerDisabled = false;
-  }
-
-  async closeBurger() {
-    await this.delay(1);
-    this.burgerDisabled = true;
-    await this.delay(120);
-    this.burgerIcon = 'menu';
-    this.burgerDisabled = false;
   }
 
   myAccountClick() {
