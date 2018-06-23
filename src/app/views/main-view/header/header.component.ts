c77ba5f8b04fe024bc5ca288a61efc4e17000997
Tim Louw <timlouw.8@gmail.com>
2018-06-23 16:48:22 +0200

    Almost finished with testimonials with async random function for random testimonial

--- src/app/views/main-view/header/header.component.ts
+++ src/app/views/main-view/header/header.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -17,4 +16,5 @@ export class HeaderComponent implements OnInit {
     this.router.navigateByUrl('/mv/(mainViews:my-account)');
   }
 
+  
 }
--- src/app/views/main-view/home/home.component.ts
+++ src/app/views/main-view/home/home.component.ts
@@ -1,15 +1,52 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  testimonialsArray = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5"
+  ]
+  counter = 1;
+  previousCounter = 1;
+  currentTestimonial = "zzz";
 
-  constructor() { }
+  constructor() { 
+    
+  }
 
   ngOnInit() {
+    this.testimonialsLoop();
   }
+
+  async testimonialsLoop() {
+    this.currentTestimonial = this.testimonialsArray[this.counter];
+    await this.delay(1000);
+    this.counter = this.randomInt(0,4);
+    if (this.previousCounter == this.counter) {
+      this.counter = this.randomInt(0,4);
+      if (this.previousCounter == this.counter) {
+        this.counter = this.randomInt(0,4);
+        if (this.previousCounter == this.counter) {
+          this.counter = this.randomInt(0,4);
+        }
+      }
+    }
+    this.previousCounter = this.counter;
+    this.testimonialsLoop();
+  }
+
+  delay(ms: number) {
+    return new Promise( resolve => setTimeout(resolve, ms) );
+  }
+
+  randomInt(min, max){
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+ }
 
 }
